40a7078563c9d3239189f73077a197ddb11c71af
ohshitnotgood <praanto@icloud.com>
2025-04-27 23:45:59 +0200

    simple websocket server

---
+++ index.ts
@@ -0,0 +1,13 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws: WebSocket) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data: WebSocket.RawData) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
