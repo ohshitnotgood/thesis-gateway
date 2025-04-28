5b8cb7d39246098d54093163563e78725db2c743
ohshitnotgood <praanto@icloud.com>
2025-04-28 16:13:19 +0200

    added echo

--- index.ts
+++ index.ts
@@ -3,12 +3,13 @@ import WebSocket, { WebSocketServer } from 'ws';
 const PORT = process.env.PORT == undefined ? 4000 : parseInt(process.env.PORT)
 const wss = new WebSocketServer({ port: PORT });
 
+let serverConnection: string | undefined = undefined
+let clientConnection: string | undefined = undefined
+
 wss.on('connection', function connection(ws: WebSocket) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data: WebSocket.RawData) {
-        console.log('received: %s', data);
+        ws.send(data)
     });
-
-    ws.send('something');
 });
